bc31dcadc49771c356979cdebd690453c0738f5a
oleglomanov <olomanov@yahoo.com>
2017-10-24 17:12:02 +0300

    Выполненное домашнее задание от лекции 1

--- src/ArrayVector.java
+++ src/ArrayVector.java
@@ -2,53 +2,67 @@ package src;
 
 public class ArrayVector {
 
-    // int elementforinput = 5;   // элемент земены
-    // int indexelementforchange = 1; // Индекс элемента для замены
-    // int indexelement1 = 2;   // номер элемента для вывода вектора1
-    // int indexelement2 = 1;   // номер элемента для вывода вектора2
-    // int v1[] = new int[]{20, 1, 4}; // Вектор 1
-    // int v2[] = new int[]{6, 16,10};   // Вектор 2
-    //
-    //
-    // int counter = 0; // Счётчик для поиска числа в масиве
-    // int item = 4;  // Число для поиска в массиве
-    // double avarage = 0; // Среднее
-    // int number = 0;
-    // int multvector = 0;
-
 
     int vectorSize;
 
-    int vector[];
+    int[] vector;
+
+    int vector2[]; // для сравнения
 
 
-    // vectorSize - Длинна масива
-    // elements - Элементы массива
+    /**
+     * @param _vectorSize - Количество элементов вектора
+     *  vector      - массив вектора 1
+     */
+
+
     ArrayVector(int _vectorSize) {
         this.vectorSize = _vectorSize;
-        vector = new int[vectorSize];
 
+        vector  = new int[vectorSize];
 
-        // i - индекс элемента
-        // return - эелемент по индексу
     }
 
+
+    /**
+     * @param i -  индекс элемента
+     * @return - элемент по индексу
+     */
     public int getElement(int i) {
         return vector[i];
     }
 
-    public String setElement(int index, int value) {
-        if (index < vector.length) {
-            vector[index] = value;
-            return value + " установлен " + " на позицию " + (index + 1);
+
+    /**
+     * @param i     -  Индекс элемента
+     * @param value -  Значение для подстановки
+     * @return новое значение
+     */
+
+    public String setElement(int i, int value) {
+        if (i < vector.length) {
+            vector[i] = value;
+            return value + " установлен " + " на позицию " + (i + 1);
         } else {
-            return "Значение индекса превышенно";
+            return "Индекс отсутствует";
         }
     }
+
+
+
+
+
+    /**
+     * @return Количество элементов
+     */
 
     public int getVectorSize() {
         return vectorSize;
     }
+
+    /**
+     * @return Минимальное значение вектора 1
+     */
 
     public int getMin() {
         int min = vector[0];
@@ -59,6 +73,9 @@ public class ArrayVector {
         return min;
     }
 
+    /**
+     * @return Максимальное значение вектора 1
+     */
 
     public int getMax() {
         int max = vector[0];
@@ -68,6 +85,12 @@ public class ArrayVector {
         }
         return max;
     }
+
+
+    /**
+     * @param element - Элемент массива
+     * @return индекс масива и вслучаем его отсутсивя -100500
+     */
 
 
     public int find(int element) {
@@ -80,6 +103,10 @@ public class ArrayVector {
     }
 
 
+    /**
+     * @return Среднее значение масива
+     */
+
     public int getAverage() {
         int sum = 0;
         for (int i : vector) {
@@ -87,6 +114,10 @@ public class ArrayVector {
         }
         return sum / vector.length;
     }
+
+    /**
+     * @return Евклидова Норма
+     */
 
     public double getNorm() {
         int norm = 0;
@@ -97,6 +128,11 @@ public class ArrayVector {
     }
 
 
+    /**
+     * @param vector1
+     * @param mult
+     */
+
     public static void mult(ArrayVector vector1, int mult) {
         for (int i = 0; i < vector1.vector.length; i++) {
             vector1.vector[i] = vector1.vector[i] * mult;
@@ -105,191 +141,64 @@ public class ArrayVector {
         System.out.println();
     }
 
+    /**
+     * Сортировка, взял от сюда - https://ru.wikibooks.org/wiki/%D0%A0%D0%B5%D0%B0%D0%BB%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D0%B8_%D0%B0%D0%BB%D0%B3%D0%BE%D1%80%D0%B8%D1%82%D0%BC%D0%BE%D0%B2/%D0%A1%D0%BE%D1%80%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%BA%D0%B0/%D0%9F%D1%83%D0%B7%D1%8B%D1%80%D1%8C%D0%BA%D0%BE%D0%BC
+     */
+
 
     public void sort() {
         for (int i = vector.length - 1; i > 0; i--) {
-            for (int j = 0; j < i; j++) {
-                if (vector[j] > vector[j + 1]) {
-                    int tmp = vector[j];
-                    vector[j] = vector[j + 1];
-                    vector[j + 1] = tmp;
+
+            for (int v = 0; v < i; v++) {
+
+                if (vector[v] > vector[v + 1]) {
+                    int tmp = vector[v];
+                    vector[v] = vector[v + 1];
+                    vector[v + 1] = tmp;
+
                 }
             }
         }
         for (int element : vector) {
             System.out.print(element + " ");
         }
-        System.out.println();
+    }
+
+    /**
+     *
+     *
+     * @param vector1 - Массиа вектора 1
+     * @param vector2 - Массив вектора 2
+     */
+
+
+    public static void sum (ArrayVector vector1, ArrayVector vector2) {
+        int sumvector [] = new int[vector1.getVectorSize()];
+        for (int i = 0; i <vector1.getVectorSize(); i++) {
+            sumvector[i]= vector1.vector[i] + vector2.vector[i];
+            }
+        for (int i1 : sumvector) {
+            System.out.print(i1 + " ");
+        }
+    }
+
+    /**
+     *
+     *
+     * @param vector1 - Массиа вектора 1
+     * @param vector2 - Массиа вектора 2
+     */
+
+    public static void  scalarvector  (ArrayVector vector1, ArrayVector vector2) {
+        int scalarvector [] = new int[vector1.getVectorSize()];
+        for (int i = 0; i <vector1.getVectorSize(); i++) {
+            scalarvector[i]= vector1.vector[i] * vector2.vector[i];
+        }
+        for (int i1 : scalarvector) {
+            System.out.print(i1 + " ");
+        }
     }
 }
-
-
-
-// вывод значений вектора 1
-// public void getVectorNumber () {
-
-
-//      for (int i : v1)
-//      {
-//          System.out.println("Значения вектора 1: " + i);}
-//  }
-
-
-//  // вывод значений вектора 2
-//  public void getVectorNumber2 () {
-//
-//  for (int i : v2)
-//  {
-//      System.out.println("Значения вектора 2: " + i); }
-//  }
-
-
-// Принт эелементов вектора1  c их индексами
-
-//      public void printElementWithIndex () {
-//              for (int i = 0; i < v1.length; i++) {
-//                  System.out.println(i + " : " + v1[i]); }
-//      }
-
-// Принт эелементов вектора1  c их индексами
-
-// public void printElementWithIndex2 () {
-//     for (int i = 0; i < v1.length; i++) {
-//         System.out.println(i + " : " + v2[i]); }
-//  }
-/**
- // Принт эелемента по индексу для вектора 1
- public void printElement1WithIndex() {
- System.out.println("\n" + "Значение вектора 1 с индексом " + indexelement1 + " равно значению: " + v1[indexelement1]);
- return  ;
- }
-
-
- // Принт эелемента по индексу для вектора 2
- public void printElement2WithIndex() {
- System.out.println("\n" + "Значение вектора 2 с индексом " + indexelement2 + " равно значению: " + v2[indexelement2]);
-
- }
-
- public void changeElement1WithIndex() {
- v1[indexelementforchange] = elementforinput;
- System.out.println("\n" + "Значения вектора 1 были измененены,новые значения:");
- for (int i : v1) {
- System.out.print(" ");
- System.out.print(i);
- }
- }
-
-
- public void changeElement2WithIndex() {
- v1[indexelementforchange] = elementforinput;
- System.out.println("\n" + "Значения вектора 2 были измененены,новые значения: ");
- for (int i : v2) {
- System.out.print(" ");
- System.out.print(i);
- }
- }
-
-
- public void printlightelement() {
- System.out.println("\n" + "Количество элементов вектора 1: " + v1.length);
- }
-
- public void printlightelement2() {
- System.out.println("\n" + "Количество элементов вектора 2: " + v2.length);
- }
-
-
-
-
-
- public void find() {
-
- for (counter = 0; counter < v1.length; counter++) {
- if (v1[counter] == item) {
- System.out.println(item + " является " + (counter + 1) + " по счету в массиве");
- // Число найдено, следовательно прекращаем поиск
- // вызовом оператора break
- break;
- }
- }
- if (counter == v1.length) {
- System.out.println("Число " + item + " не найдено в массиве");
- }
-
- }
-
- public void getAverage() {
- for (int i = 0; i < v1.length; i++) {
- avarage += v1[i];
- }
- System.out.println(avarage / v1.length);
- }
-
- public void getNorm() {
- for (int inum : v1) {
- number = inum;
- int s = 1;
- for (int i = 0; i < 2; ++i) //
- {
- s = s * number;
-
- // System.out.println("типа среднее " + avarage);
- }
- avarage += s;
- // System.out.println("Результат возведниея в степень чисел первого вектора:  " + s);
- }
- System.out.println("Евклидова норма первого вектора:" + Math.sqrt(avarage));
- }
-
- // Сортировка от большего к меньшему
- public void sort() {
-
- for (int i = 0; i < v1.length; i++) {
- for (int j = i; j < v1.length; j++) {
- if (v1[i] > v1[j]) {
- int tmp = v1[i];
- v1[i] = v1[j];
- v1[j] = tmp;
- }
- }
- System.out.println(v1[i]);
- }
- }
-
- // перемножение первого вектора на число mult
- public void mult() {
- for (int inum : v1) {
- number = inum;
- int mult = 2;
- int s = 0;
- for (int i = 0; i < 2; ++i) //
- {
- s = mult * number;
- }
- System.out.println(s);
-
- }
- }
-
- // Сложение первого вектора на второй
-
- public void sumV1OnV2() {
- if (v1.length == v2.length) {
-
- for (int i = 0; i < v1.length; i++) {
-
- multvector = v1[i] + v2[i];
- System.out.println("Результат сложения элеменетов: "  + multvector);
- }
- }
- else{
- System.out.println("Вектора не равны");
- }
- }
- }
-
- */
 
 
 
--- src/ArrayVectorMain.java
+++ src/ArrayVectorMain.java
@@ -6,8 +6,13 @@ public class ArrayVectorMain {
     public static void main(String[] args)  {
 
 
-        ArrayVector vector1 = new ArrayVector(6);
-        ArrayVector vector2 = new ArrayVector(10);
+        ArrayVector vector1 = new ArrayVector(4);
+        ArrayVector vector2 = new ArrayVector(4);
+
+        /**
+         *
+         *  Заполняю первый элемент
+         */
 
         for (int i = 0; i < vector1.getVectorSize(); i++) {
             double r = Math.random()*100;
@@ -18,6 +23,23 @@ public class ArrayVectorMain {
             }
         }
 
+        /**
+         *
+         * Заполняю второй элемент
+         *
+         */
+
+
+        for (int i = 0; i < vector2.getVectorSize(); i++) {
+            double r = Math.random()*100;
+            int b = (int)r;
+            vector2.setElement(i, b);
+            {
+
+                System.out.println(" Все элементы вектора 2 : " + vector2.getElement(i));
+            }
+        }
+
         System.out.println(" Элемент по заданному индексу для вектора 1 : " + vector1.getElement(3));
         System.out.println(" Заданный элемент для вектора 1 : "+ vector1.setElement(1,17));
         System.out.println(" Длинна массива : "+ vector1.getVectorSize() + " элементов");
@@ -25,35 +47,11 @@ public class ArrayVectorMain {
         System.out.println(" Максимальный элемент : "+ vector1.getMax());
         System.out.println(" Поиск индекса элемена : " + vector1.find(17));
         System.out.println(" Среднее арифметическое  : " + vector1.getAverage());
-        System.out.println("Евклидова норма  : " + vector1.getNorm());
-        System.out.println("умножения вектора на число : "); ArrayVector.mult(vector1,5) ;
-        System.out.println("сортировки вектора : ");vector1.sort();
-
+        System.out.println(" Евклидова норма  : " + vector1.getNorm());
+        System.out.println(" умножения вектора на число : "); ArrayVector.mult(vector1,5) ;
+        System.out.println(" Cортировки вектора : ");vector1.sort();
+        System.out.println("\n"+ "Результат сложения векторов  :");ArrayVector.sum(vector1,vector2);
+        System.out.println("\n"+ "Результат скалярного произведения двух векторов:");ArrayVector.scalarvector(vector1,vector2);
 
     }
 }
-
-
-
-/**   можно удалить будет потом если хоть  заработает другой вариант
-
- vector_vector.printElement1WithIndex();
- vector_vector.printElement2WithIndex();
- vector_vector.changeElement1WithIndex();
- vector_vector.changeElement2WithIndex();
- vector_vector.printlightelement();
- vector_vector.printlightelement2();
-
- vector_vector.maxAndMin();
- vector_vector.find ();
- vector_vector.getAverage();
- vector_vector.getNorm();
- vector_vector.sort();
- vector_vector.mult();
- vector_vector.sumV1OnV2();
- */
-
-
-
-
-
